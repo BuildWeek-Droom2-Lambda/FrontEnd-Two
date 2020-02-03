170be6480bd0f50827cc5a606f78bdd90fd0952a
Hutch Crowley <tothecoda@gmail.com>
2020-02-03 03:52:20 -0600

    Update add  Router component to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import reactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "App.css";
 
 import Routes from "./Routes";
 
-reactDOM.render(<Routes />, document.getElementById("root"));
+reactDOM.render(
+  <Router>
+    <Routes />
+  </Router>,
+  document.getElementById("root")
+);
